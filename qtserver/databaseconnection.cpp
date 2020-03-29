75f8ad2be1559e2e9e406f892451447be05a4b34
p.anna9603 <p.anna9603@gmail.com>
2020-03-29 20:08:06 +0200

    régi-új adatbázis kapcsolat

--- qtserver/databaseconnection.cpp
+++ qtserver/databaseconnection.cpp
@@ -6,15 +6,33 @@ QSqlDatabase DatabaseConnection::getDb() const
     return db;
 }
 
+//DatabaseConnection::DatabaseConnection()
+//{
+//    qDebug() << "DataBaseConnection() fv\n";
+//    //http://www.phpmyadmin.co/index.php    <-Server
+//    db=QSqlDatabase::addDatabase("QMYSQL");
+//    db.setHostName("sql2.freesqldatabase.com");
+//    db.setUserName("sql2326670");      // <-Username
+//    db.setPassword("bG5%uY1!");         //<-Password
+//    db.setDatabaseName("sql2326670");
+//    if(db.open())
+//    {
+//       qDebug()<<"Connection, Database connected successfully";
+//    }
+//    else
+//    {
+//        qDebug()<<"Not Connected, Database not connected";
+//        qDebug()<<db.lastError();
+//    }
+//}
 DatabaseConnection::DatabaseConnection()
 {
-    qDebug() << "DataBaseConnection() fv\n";
     //http://www.phpmyadmin.co/index.php    <-Server
     db=QSqlDatabase::addDatabase("QMYSQL");
-    db.setHostName("sql2.freesqldatabase.com");
-    db.setUserName("sql2326670");      // <-Username
-    db.setPassword("bG5%uY1!");         //<-Password
-    db.setDatabaseName("sql2326670");
+    db.setHostName("remotemysql.com");
+    db.setUserName("GaZL22lGP6");      // <-Username
+    db.setPassword("X5QBXJILeg");         //<-Password
+    db.setDatabaseName("GaZL22lGP6");
     if(db.open())
     {
        qDebug()<<"Connection, Database connected successfully";
